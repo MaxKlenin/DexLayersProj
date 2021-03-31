31bd7bf0ac5fc705d71c92be0504ec1e8edfb4f9
Max <maximklllll@gmail.com>
2021-03-31 04:33:49 +0300

    ....

--- ObjectTests/DictionaryTest.cs
+++ ObjectTests/DictionaryTest.cs
@@ -14,8 +14,6 @@ namespace ObjectTests
 
         private static Stopwatch stopwatch = new Stopwatch();
 
-
-
         private Dictionary<Person, string> GeneratePersonsDictionary(int count)
         {
             if (count<=0)
@@ -74,12 +72,112 @@ namespace ObjectTests
                 catch (ArgumentException)
                 {
 
-                    Console.WriteLine("#GeneratePersonsDictionary error# ArgumentException при добавление эл-а в словарь");
+                    Console.WriteLine("#GeneratePersonsDictionaryWithHandMadeHash error# " +
+                        "ArgumentException при добавление эл-а в словарь");
                 }
             }
             return dict;
         }
 
-        
+        private List<Person> GeneratePersonsList(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("#GeneratePersonsDictionary error# " +
+                    "присвоенно неверное знаечение параметру count");
+            }
+
+            stopwatch.Reset();
+
+            var list = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    var person = GeneratorFromPerson.GeneratePerson();
+
+                    stopwatch.Start();
+                    list.Add(person);
+                    stopwatch.Stop();
+                }
+                catch (ArgumentException)
+                {
+
+                    Console.WriteLine("#GeneratePersonsList error# ArgumentException при добавление эл-а в словарь");
+                }
+            }
+            return list;
+        }
+
+        private List<PersonHandMadeHash> GeneratePersonsListWithHandMadeHash(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("#GeneratePersonsDictionary error# " +
+                    "присвоенно неверное знаечение параметру count");
+            }
+
+            stopwatch.Reset();
+
+            var list = new List<PersonHandMadeHash>();
+
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    var person = GeneratorFromPerson.GeneratePersonHandMadeHash();
+
+                    stopwatch.Start();
+                    list.Add(person);
+                    stopwatch.Stop();
+                }
+                catch (ArgumentException)
+                {
+
+                    Console.WriteLine("#GeneratePersonHandMadeHash error# ArgumentException при добавление эл-а в словарь");
+                }
+            }
+            return list;
+        }
+
+        private bool FindInDictionary(Dictionary<Person, string> dictionary, Person person)
+        {
+            stopwatch.Restart();
+            bool result = dictionary.ContainsKey(person);
+            stopwatch.Stop();
+            return result;
+        }
+
+        private bool FindInDictionary(Dictionary<PersonHandMadeHash, string> dictionary, PersonHandMadeHash person)
+        {
+            stopwatch.Restart();
+            bool result = dictionary.ContainsKey(person);
+            stopwatch.Stop();
+            return result;
+        }
+
+        private bool FindInList(PersonHandMadeHash person, List<PersonHandMadeHash> list)
+        {
+            stopwatch.Restart();
+            bool result = list.Contains(person);
+            stopwatch.Stop();
+            return result;
+        }
+
+        private bool FindInList(Person person, List<Person> list)
+        {
+            stopwatch.Restart();
+            bool result = list.Contains(person);
+            stopwatch.Stop();
+            return result;
+        }
+
+        private void SetInstanceCount(int count)
+        {
+
+        }
+
+
     }
 }
